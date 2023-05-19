0456c61a8969691c51dd0169fbba485fa7331abf
HuyVuong <vuonghuy12345@gmail.com>
2023-05-19 21:47:00 +0700

    responsive Home Page, Header ,footer , Movie Page

--- src/Layouts/Components/Header/Header.jsx
+++ src/Layouts/Components/Header/Header.jsx
@@ -3,7 +3,7 @@ import styles from './Header.module.scss/';
 import HeadlessTippy from '@tippyjs/react/headless';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faTimes, faUser, faUserCircle } from '@fortawesome/free-solid-svg-icons';
 import Search from '../Search/Search';
 
 const cx = classNames.bind(styles);
@@ -76,75 +76,130 @@ function Header() {
         'United Kingdom',
         'United States',
     ];
+    const show = () => {
+        const modalMobile = document.getElementById('modal-mobile');
+        modalMobile.classList.add(`${cx('open')}`);
+    };
+    function close() {
+        const modalMobile = document.getElementById('modal-mobile');
+        modalMobile.classList.remove(`${cx('open')}`);
+    }
     return (
-        <div className={cx('Header-Wrapper')}>
-            <nav className={cx('Header-nav')}>
-                <div className={cx('Header-logo')}>
-                    <img src="https://s1.bunnycdn.ru/assets/sites/flixtor/logo.png" alt="" />
-                    <h1 className={cx('Header-logo-title')}>FlixTor.video</h1>
-                </div>
-                <ul className={cx('Header-nav-list')}>
-                    <Link to={'/'}>
-                        <li className={cx('Header-nav-list-item')}>Home</li>
-                    </Link>
-                    <div>
-                        <HeadlessTippy
-                            interactive
-                            placement="bottom-start"
-                            render={(attrs) => (
-                                <ul className={cx('genre-child')} tabIndex="-1" {...attrs}>
-                                    {genre.map((item, index) => (
-                                        <Link key={index} to="/" className={cx('genre-child-link')}>
-                                            <li className={cx('genre-child-item')}>{item}</li>
-                                        </Link>
-                                    ))}
-                                </ul>
-                            )}
-                        >
-                            <li className={cx('Header-nav-list-item')}>genre</li>
-                        </HeadlessTippy>
+        <>
+            <div className={cx('Header-Wrapper')}>
+                <nav className={cx('Header-nav')}>
+                    <div className={cx('Header-logo')}>
+                        <img src="https://s1.bunnycdn.ru/assets/sites/flixtor/logo.png" alt="" />
+                        <h1 className={cx('Header-logo-title')}>FlixTor.video</h1>
                     </div>
+                    <ul className={cx('Header-nav-list')}>
+                        <Link to={'/'}>
+                            <li className={cx('Header-nav-list-item')}>Home</li>
+                        </Link>
+                        <div>
+                            <HeadlessTippy
+                                interactive
+                                placement="bottom-start"
+                                render={(attrs) => (
+                                    <ul className={cx('genre-child')} tabIndex="-1" {...attrs}>
+                                        {genre.map((item, index) => (
+                                            <Link key={index} to="/" className={cx('genre-child-link')}>
+                                                <li className={cx('genre-child-item')}>{item}</li>
+                                            </Link>
+                                        ))}
+                                    </ul>
+                                )}
+                            >
+                                <li className={cx('Header-nav-list-item')}>genre</li>
+                            </HeadlessTippy>
+                        </div>
 
-                    <div>
-                        <HeadlessTippy
-                            interactive
-                            placement="bottom-start"
-                            render={(attrs) => (
-                                <ul className={cx('country-child')} tabIndex="-1" {...attrs}>
-                                    {countrys.map((item, index) => (
-                                        <Link key={index} to="/" className={cx('country-child-link')}>
-                                            <li className={cx('country-child-item')}>{item}</li>
-                                        </Link>
-                                    ))}
-                                </ul>
-                            )}
-                        >
-                            <li className={cx('Header-nav-list-item')}>country</li>
-                        </HeadlessTippy>
+                        <div>
+                            <HeadlessTippy
+                                interactive
+                                placement="bottom-start"
+                                render={(attrs) => (
+                                    <ul className={cx('country-child')} tabIndex="-1" {...attrs}>
+                                        {countrys.map((item, index) => (
+                                            <Link key={index} to="/" className={cx('country-child-link')}>
+                                                <li className={cx('country-child-item')}>{item}</li>
+                                            </Link>
+                                        ))}
+                                    </ul>
+                                )}
+                            >
+                                <li className={cx('Header-nav-list-item')}>country</li>
+                            </HeadlessTippy>
+                        </div>
+                        <Link to={'/movie?page=1'}>
+                            <li className={cx('Header-nav-list-item')}>movies</li>
+                        </Link>
+                        <Link to="/">
+                            <li className={cx('Header-nav-list-item')}>tv shows</li>
+                        </Link>
+                        <Link to="/">
+                            <li className={cx('Header-nav-list-item')}>top imdb</li>
+                        </Link>
+                    </ul>
+
+                    <div className={cx('Header-login-register')}>
+                        <FontAwesomeIcon icon={faUser} />
+                        <span>Login/ </span>
+                        <span>Register</span>
                     </div>
-                    <Link to={'/movie?page=1'}>
-                        <li className={cx('Header-nav-list-item')}>movies</li>
-                    </Link>
-                    <Link to="/">
-                        <li className={cx('Header-nav-list-item')}>tv shows</li>
-                    </Link>
-                    <Link to="/">
-                        <li className={cx('Header-nav-list-item')}>top imdb</li>
-                    </Link>
-                </ul>
+                </nav>
 
-                <div className={cx('Header-login-register')}>
-                    <FontAwesomeIcon icon={faUser} />
-                    <span>Login/ </span>
-                    <span>Register</span>
+                <div className={cx('Header-big-title-wrapper')}>
+                    <h1 className={cx('Header-big-title')}>Find Movies, TV shows and more</h1>
                 </div>
-            </nav>
-
-            <div className={cx('Header-big-title-wrapper')}>
-                <h1 className={cx('Header-big-title')}>Find Movies, TV shows and more</h1>
+                <Search />
             </div>
-            <Search />
-        </div>
+
+            {/* mobile */}
+            <div className={cx('Header-Wrapper-mobile')}>
+                <nav className={cx('Header-mobile-nav')}>
+                    <FontAwesomeIcon icon={faBars} className={cx('Header-mobile-bars')} onClick={show} />
+                    <div className={cx('Header-mobile-logo')}>
+                        <img
+                            className={cx('header-mobile-logo-img')}
+                            src="https://s1.bunnycdn.ru/assets/sites/flixtor/logo.png"
+                            alt=""
+                        />
+                        <h1 className={cx('Header-mobile-logo-title')}>FlixTor.video</h1>
+                    </div>
+                    <FontAwesomeIcon icon={faUserCircle} className={cx('Header-mobile-user')} />
+                </nav>
+                <Search />
+            </div>
+
+            {/* modal mobile */}
+            <div className={cx('modal-mobile')} id="modal-mobile">
+                <div className={cx('modal-overlay')} id="modal-overlay" onClick={close}></div>
+                <div className={cx('modal-body')}>
+                    <FontAwesomeIcon icon={faTimes} className={cx('close-icon')} id="close-icon" onClick={close} />
+                    <ul className={cx('nav2-list-mobile')}>
+                        <li className={cx('nav2-list-item-mobile')} onClick={close}>
+                            <Link to="/">HOME</Link>
+                        </li>
+                        <li className={cx('nav2-list-item-mobile')} onClick={close}>
+                            <Link to="/">GENRE</Link>
+                        </li>
+                        <li className={cx('nav2-list-item-mobile')} onClick={close}>
+                            <Link to="/">COUNTRY</Link>
+                        </li>
+                        <li className={cx('nav2-list-item-mobile')} onClick={close}>
+                            <Link to="/movie?page=1">MOVIES</Link>
+                        </li>
+                        <li className={cx('nav2-list-item-mobile')} onClick={close}>
+                            <Link to="/">TV SHOWS</Link>
+                        </li>
+                        <li className={cx('nav2-list-item-mobile')} onClick={close}>
+                            <Link to="/">TOP IMDB</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </>
     );
 }
 
