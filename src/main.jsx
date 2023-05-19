3f936fc504c4777d21b61f1e41caf97827e2b858
HuyVuong <vuonghuy12345@gmail.com>
2023-05-19 16:36:40 +0700

    update file jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import GlobalStyles from '../Components/GlobalStyles/GlobalStyles.js';
+import GlobalStyles from '../Components/GlobalStyles/GlobalStyles.jsx';
 import { Provider } from 'react-redux';
 import store from './redux/store.jsx';
 
